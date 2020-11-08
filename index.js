c00561bd74e85423deb4fab4166a1a7cfa64baca
Romario Gustave <romagustave10@gmail.com>
2020-11-08 19:21:56 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,6 +2,18 @@ function wakeDog(dogName, dogBreed) {
   console.log(`Wake ${dogName} the ${dogBreed}`);
 }
 
-function leashdog(dogName, dogBreed){
+function leashDog(dogName, dogBreed){
+console.log(`leash ${dogName} the ${dogBreed}`);
+}
+
+function walkToPark(dogName, dogBreed){
+console.log(`walk ${dogName} the ${dogBreed} to park`);
+}
+
+function throwFrisbee(dogName, dogBreed){
+console.log(`throw frisbee ${dogName} the ${dogBreed}`);
+}
+
+function walkHome(dogName, dogBreed){
 console.log(`leash ${dogName} the ${dogBreed}`);
 }
